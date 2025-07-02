03273ec56bb1ada0fe8c526ced008b6755a01d2a
Vasil Vasilev <vagrivas08@gmail.com>
2025-07-02 19:59:39 +0300

    Feature Projection Layer

---
+++ context/feature_projection.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class FeatureProjection(nn.Module):
+    def __init__(self, in_channels, out_channels = 768): # 768 - BASE, 1024 - LARGE
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.projection_conv = nn.Conv1d(in_channels, out_channels, 1)
+    
+    def forward(self, x):
+        x = self.projection_conv(x)
+        return x
+    
+if __name__ == "__main__":
+    x = torch.randn(4, 512, 760)
+    model = FeatureProjection(512)
+    out = model(x) 
+    print(out.shape)
