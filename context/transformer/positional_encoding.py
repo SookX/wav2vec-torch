199145c95b6b3442f4f6fa586ee43f7abcb3ce9d
Vasil Vasilev <vagrivas08@gmail.com>
2025-07-02 20:39:15 +0300

    Positional Encoding

--- context/transformer/positional_encoding.py
+++ context/transformer/positional_encoding.py
@@ -2,4 +2,13 @@ import torch
 import torch.nn as nn
 
 class PositionalEncoding(nn.Module):
-    pass
+    def __init__(self, in_channels=768, num_groups=16):
+        super().__init__()  
+        self.in_channels = in_channels
+        self.num_groups = num_groups
+
+        self.g_conv = nn.Conv1d(in_channels, in_channels, kernel_size=1, groups=num_groups)
+
+    def forward(self, x):
+        pos_emb = self.g_conv(x)
+        return x + pos_emb
